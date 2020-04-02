508307d34cf97d44e264fb3ba51a2cf7e0e8ec96
psialedev <psialedev@gmail.com>
2020-04-02 13:21:21 -0600

    integrated nokogiri gem to output some of the results of the query

--- main.rb
+++ main.rb
@@ -7,14 +7,14 @@ class BingSearcher
         @resource = 'https://www.bing.com/search'
     end
 
-    def search
-        puts 'insert a search query, please'
-        query = gets.chomp until !query.nil?
+    def search(query)
         RestClient.get @resource, {params: {q:query, answerCount: 1}}
     end
 end
 
 searcher = BingSearcher.new
-# puts searcher.search('Hello').body
-nokogiri = Nokogiri::HTML.parse(searcher.search.body)
-nokogiri.xpath("//li[@class ='b_algo']/h2/a").each { |link| puts link}
+ puts searcher.search('Hello').body
+  puts 'insert a search query, please'
+query = gets.chomp
+nokogiri = Nokogiri::HTML.parse(searcher.search(query).body)
+nokogiri.xpath("//li[@class ='b_algo']/h2/a/@href").each { |link| puts link}
