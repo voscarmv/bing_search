0f1e29f9d576d07015220e29179f6b3d521f412c
psialedev <psialedev@gmail.com>
2020-04-02 11:46:43 -0600

    playing around with the gem printing headers, cookies and code response

--- main.rb
+++ main.rb
@@ -6,10 +6,13 @@ class BingSearcher
     end
 
     def search(query)
-        RestClient.get @resource, {params: {q:query}}
+        RestClient.get @resource, {params: {q:query, answerCount: 1}}
     end
 end
 
 searcher = BingSearcher.new
-puts searcher.search('Hello').body
-
+# puts searcher.search('Hello').body
+puts searcher.search('World').code
+puts searcher.search('World').headers
+puts searcher.search('World').raw_headers
+puts searcher.search('World').cookies
