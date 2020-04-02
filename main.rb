08361dca9e094e399ef8985d8627cd9b29ae056d
psialedev <psialedev@gmail.com>
2020-04-02 14:04:37 -0600

    tested different gem functions and added a terminal output with the query results

--- main.rb
+++ main.rb
@@ -8,13 +8,20 @@ class BingSearcher
     end
 
     def search(query)
-        RestClient.get @resource, {params: {q:query, answerCount: 1}}
+        RestClient.get @resource, {params: {q:query, count: 10}}
     end
 end
 
+
+
 searcher = BingSearcher.new
- puts searcher.search('Hello').body
-  puts 'insert a search query, please'
+puts 'insert a search query, please'
 query = gets.chomp
+searcher.search(query).cookies
+searcher.search(query).headers
+searcher.search(query).raw_headers
+searcher.search(query).request
+searcher.search(query).code
 nokogiri = Nokogiri::HTML.parse(searcher.search(query).body)
-nokogiri.xpath("//li[@class ='b_algo']/h2/a/@href").each { |link| puts link}
+nokogiri.css("//li[@class ='b_algo']/h2/a/@href").each { |link| puts link}
+
