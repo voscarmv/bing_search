f94d4f87d2502fc98b35c284c4aaccaf81dfe4e1
psialedev <psialedev@gmail.com>
2020-04-02 10:52:35 -0600

    created git flow and Bingsearcher class

--- main.rb
+++ main.rb
@@ -1,1 +1,10 @@
-puts 'Hello, World!'
+require 'rest-client'
+class BingSearcher
+@resource = 'https://www.bing.com/search?p='
+
+
+def search(query)
+    RestClient.get resource, {params: {q:query}}.body
+end
+puts search('Hello')
+end
